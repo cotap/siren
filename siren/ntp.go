1e1f37ad102910eaca0b1f9531db665d19543750
Paul Hamera <paul@zinc.it>
2017-04-25 16:47:36 -0700

    Add NTP drift check

---
+++ siren/ntp.go
@@ -0,0 +1,39 @@
+package siren
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"strconv"
+	"strings"
+)
+
+func NTP(w, f int) Status {
+	out, err := exec.Command("ntpq", "-c", "rv 0 offset").Output()
+	if err != nil {
+		fmt.Fprintf(os.Stdout, "\n%s: unable to determine NTP drift. %s\n", warn, err)
+		return warn
+	}
+
+	// Output: offset=72.062
+	floatStr := strings.Trim(string(out[7:]), "\n")
+	drift, err := strconv.ParseFloat(floatStr, 64)
+	if err != nil {
+		fmt.Fprintf(os.Stdout, "\n%s: unable to determine NTP drift\n", warn)
+		return warn
+	}
+
+	fmt.Fprintf(os.Stdout, "NTP drift: %0.2fms\n", drift)
+
+	if drift >= float64(f) {
+		fmt.Fprintf(os.Stdout, "\n%s: NTP drift exceeds threshold (%0.2fms >= %dms)\n", fail, drift, f)
+		return fail
+	}
+
+	if drift >= float64(w) {
+		fmt.Fprintf(os.Stdout, "\n%s: NTP drift exceeds threshold (%0.2fms >= %dms)\n", warn, drift, w)
+		return warn
+	}
+
+	return ok
+}
